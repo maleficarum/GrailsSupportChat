0a5732785762c8c4bdc674fec2336880082baaae
Oscar Ivan Hernandez Ventura <oscar@angellore.com.mx>
2012-02-20 00:04:52 -0600

    Adde jquery ui for main interface

--- grails-app/taglib/SupportChatTagLib.groovy
+++ grails-app/taglib/SupportChatTagLib.groovy
@@ -2,16 +2,27 @@
 class SupportChatTagLib {
 
 	static namespace = "mm"
-	def jQueryVersion = "1.7.1"
-	def jQueryUrl = "http://code.jquery.com/jquery-"
 	
+	/**
+	* This closure is used if you want to add our jquery version. If you want to provide
+	* your own jquery version, not use this closure.
+	*/
 	def jquery = { attrs, body ->
-		if(attrs?.version) {
-			jQueryVersion = attrs.version
-		}
-		out << "<script src=\"${jQueryUrl}${jQueryVersion}.min.js\"></script>"
+		out << "<script src=\"js/jquery-1.7.1.min.js\"></script>"
 	}
 	
+	/**
+	* This closure is used if you want to add our jqueryui version. If you want to provide
+	* your own jqueryui version, not use this closure.
+	*/	
+	def jqueryui = { attrs, body ->
+		out << "<script src=\"js/jquery-ui-1.8.17.custom.min.js\"></script>"		
+		out << "<link rel='stylesheet' href='css/jquery-ui-1.8.17.custom.css' />"			
+	}
+	
+	/**
+	* Creates the main chat window
+	*/	
 	def supportChat = { attrs, body ->
 		out << render(template:"/templates/chatWindow", plugin:"supportChat")
 //	    out << body()
--- grails-app/views/templates/_chatWindow.gsp
+++ grails-app/views/templates/_chatWindow.gsp
@@ -1,1 +1,9 @@
-<b>Chat window</b>
+<div id="mmsupportchatw" title="Basic dialog">
+	<p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+</div>
+
+<script>
+$(function() {
+		$( "#mmsupportchatw" ).dialog({ position: 'bottom', closeOnEscape: false });
+});
+</script>
